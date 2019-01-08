5b8f9da61452477602cbb053e4aa434294e88cf0
Christopher Moore <christopher.e.moore@gmail.com>
2019-01-08 16:50:48 +0800

    draw on spheres

--- run.lua
+++ run.lua
@@ -9,10 +9,8 @@ local matrix = require 'matrix'
 
 --local boundaries = assert(json.decode(file['tectonicplates/GeoJSON/PB2002_boundaries.json']))
 --local orogens = assert(json.decode(file['tectonicplates/GeoJSON/PB2002_orogens.json']))
---local plates = assert(json.decode(file['tectonicplates/GeoJSON/PB2002_plates.json']))
 --local steps = assert(json.decode(file['tectonicplates/GeoJSON/PB2002_steps.json']))
 --local coastline = assert(json.decode(file['naturalearthdata/ne_10m_coastline.geojson']))
-local land = assert(json.decode(file['naturalearthdata/ne_10m_land.geojson']))
 
 local Orbit = require 'glapp.orbit'
 local View = require 'glapp.view'
@@ -29,60 +27,170 @@ function App:initGL(...)
 end
 
 local layers = {
-	--{name='boundaries', data=boundaries, color={1,0,0}},
-	--{name='orogens', data=orogens, color={1,1,0}},
-	--{name='plates', data=plates, color={0,1,0}},
-	--{name='steps', data=steps, color={0,1,1}},
-	--{name='coastline', data=coastline, color={0,1,1}},
-	{name='land', data=land, color={0,1,1}},
+	--{name='boundaries', data=boundaries},
+	--{name='orogens', data=orogens},
+	--{name='plates', data=assert(json.decode(file['tectonicplates/GeoJSON/PB2002_plates.json']))},
+	--{name='steps', data=steps},
+	--{name='coastline', data=coastline},
+	{name='land', data=assert(json.decode(file['naturalearthdata/ne_10m_land.geojson']))},
 }
+
+
+local function calcFeatureCOM(poly)
+	local com = matrix{0,0}
+	local area = 0
+	local n = #poly
+	assert(n > 2)
+	for i=1,n do
+		local i2 = i % n + 1
+		
+		local a = matrix(poly[i])
+		local b = matrix(poly[i2])
+		local triarea = .5 * matrix{a,b}:det()
+		com = com + (a + b) * triarea
+		area = area + triarea
+	end
+	com = com / (3 * area)
+	return com, area
+end
+
+for _,layer in ipairs(layers) do
+	local com = matrix{0,0}
+	local mass = 0
+	for _,feature in ipairs(layer.data.features) do
+		local fcom = matrix{0,0}
+		local fmass = 0
+		if feature.geometry.type == 'LineString' then
+			fcom, fmass = calcFeatureCOM(feature.geometry.coordinates)
+		elseif feature.geometry.type == 'Polygon' then
+			for _,poly in ipairs(feature.geometry.coordinates) do
+				local pcom, pmass = calcFeatureCOM(poly)
+				poly.com = pcom
+				poly.mass = pmass
+				fcom = fcom + pcom * pmass
+				fmass = fmass + pmass
+			end
+			fcom = fcom / fmass
+		elseif feature.geometry.type == 'MultiPolygon' then
+			for _,group in ipairs(feature.geometry.coordinates) do
+				for _,poly in ipairs(group) do
+					local pcom, pmass = calcFeatureCOM(poly)
+					poly.com = pcom
+					poly.mass = pmass
+					fcom = fcom + pcom * pmass
+					fmass = mass + pmass
+				end
+			end
+			fcom = fcom / fmass
+		else
+			error('here')
+		end
+		feature.com = fcom
+		feature.mass = fmass
+		com = com + fcom * fmass
+		mass = mass + fmass
+	end
+	com = com / mass
+	layer.com = com
+	layer.mass = mass
+end
+
+_G.drawOnSphere = false
+_G.drawCOMs = false
+
+local function spherexform(lon, lat)
+	local inverseFlattening = 298.257223563
+	local equatorialRadius = 1
+	local height = 0
+
+	local phi = math.rad(lat)
+	local lambda = math.rad(lon)
+	local cosPhi = math.cos(phi)
+	local sinPhi = math.sin(phi)
+	local eccentricitySquared = (2 * inverseFlattening - 1) / (inverseFlattening * inverseFlattening)
+	local sinPhiSquared = sinPhi * sinPhi
+	local N = equatorialRadius / math.sqrt(1 - eccentricitySquared * sinPhiSquared)
+	local NPlusH = N + height
+	return 
+		NPlusH * cosPhi * math.cos(lambda),
+		NPlusH * cosPhi * math.sin(lambda),
+		(N * (1 - eccentricitySquared) + height) * sinPhi
+end
+
+local function vertex(coord)
+	if drawOnSphere then
+		gl.glVertex3f(spherexform(table.unpack(coord)))
+	else
+		gl.glVertex2f(coord[1], coord[2])
+	end
+end
+
+local latmin = math.huge
+local latmax = -math.huge
+local lonmin = math.huge
+local lonmax = -math.huge
+
+local function drawPoly(poly)
+	if not poly.color then
+		poly.color = matrix{math.random(), math.random(), math.random()}
+		poly.color = poly.color / poly.color:norm()
+	end	
+	gl.glColor3f(table.unpack(poly.color))						
+	
+	gl.glBegin(gl.GL_LINE_STRIP)
+	for _,coord in ipairs(poly) do
+		local lon, lat = table.unpack(coord)
+		latmin = math.min(latmin, lat)
+		latmax = math.max(latmax, lat)
+		lonmin = math.min(lonmin, lon)
+		lonmax = math.max(lonmax, lon)
+
+		vertex(coord)
+	end
+	gl.glEnd()	
+
+	if drawCOMs then
+		assert(poly.com)
+		if poly.com then
+			gl.glBegin(gl.GL_LINES)
+			--for _,coord in ipairs(poly) do
+			local n = 30
+			for i=1,n do
+				local j = math.floor((i-1)/(n-1)*(#poly-1))+1
+				local coord = poly[j]
+
+				vertex(coord)
+				gl.glVertex3f(poly.com[1] or 0, poly.com[2] or 0, poly.com[3] or 0)
+			
+			end
+			gl.glEnd()
+		end
+	end
+end
+
+--layers[1].data.features = table.sub(layers[1].data.features, 1, 1)
 
 function App:update(...)
 	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
-
+	
 	for _,layer in ipairs(layers) do
 		if not layer.disabled then		
 			for _,feature in ipairs(layer.data.features) do
-				if not feature.disabled then
-					
-					if not feature.color then
-						feature.color = matrix{math.random(), math.random(), math.random()}
-						feature.color = feature.color / feature.color:norm()
-					end	
-					gl.glColor3f(table.unpack(feature.color))
-
-					local polygon = gl.GL_POLYGON
-					if feature.gl_drawlines then
-						polygon = gl.GL_LINE_LOOP
+				if feature.geometry.type == 'LineString' then
+					drawPoly(feature.geometry.coordinates)
+				elseif feature.geometry.type == 'Polygon' then
+					assert(#feature.geometry.coordinates == 1)
+					for _,poly in ipairs(feature.geometry.coordinates) do
+						drawPoly(poly)
 					end
-	
-					if feature.geometry.type == 'LineString' then
-						gl.glBegin(gl.GL_LINE_STRIP)
-						for _,coord in ipairs(feature.geometry.coordinates) do
-							gl.glVertex2f(coord[1], coord[2])
-						end
-						gl.glEnd()	
-					elseif feature.geometry.type == 'Polygon' then
-						for _,poly in ipairs(feature.geometry.coordinates) do
-							gl.glBegin(polygon)
-							for _,coord in ipairs(poly) do
-								gl.glVertex2f(coord[1], coord[2])
-							end
-							gl.glEnd()	
-						end
-					elseif feature.geometry.type == 'MultiPolygon' then
-						for _,group in ipairs(feature.geometry.coordinates) do
-							for _,poly in ipairs(group) do
-								gl.glBegin(polygon)
-								for _,coord in ipairs(poly) do
-									gl.glVertex2f(coord[1], coord[2])
-								end
-								gl.glEnd()	
-							end		
+				elseif feature.geometry.type == 'MultiPolygon' then
+					for _,group in ipairs(feature.geometry.coordinates) do
+						for _,poly in ipairs(group) do
+							drawPoly(poly)
 						end		
-					else
-						error("unknown geometry.type "..feature.geometry.type)
-					end
+					end		
+				else
+					error("unknown geometry.type "..feature.geometry.type)
 				end
 			end
 		end
@@ -100,21 +208,27 @@ local function checkbox(t, k)
 end
 
 function App:updateGUI(...)
+	checkbox(_G, 'drawOnSphere')
+	checkbox(_G, 'drawCOMs')
+	
+	self.view.ortho = not drawOnSphere
+	
 	for _,layer in ipairs(layers) do
+		local com = layer.com
+		local mass = layer.mass
 		ig.igPushIDStr(layer.name)
-		ig.igText(layer.name)
-		for _,k in ipairs{'disabled'} do
-			checkbox(layer, k)
-		end
-		
-		for _,feature in ipairs(layer.data.features) do
-			for _,k in ipairs{'disabled', 'gl_drawlines'} do
-				checkbox(feature, k)
+		ig.igText(layer.name..' area '..mass)
+		ig.igSameLine()
+		if ig.igCollapsingHeader'' then
+			for _,k in ipairs{'disabled'} do
+				checkbox(layer, k)
 			end
 		end
-		
 		ig.igPopID()
 	end
 end
 
 App():run()
+
+print('lat', latmin, latmax)
+print('lon', lonmin, lonmax)
